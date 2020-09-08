5d5152b52871088a2abf2124432f6ebf864c01f9
f3ath <karapetov@gmail.com>
2020-09-08 00:09:14 -0700

    0.0.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+## [0.0.4] - 2020-09-08
+### Fixed
+- KeepPreRelease wrapper strips the pre-release tag to allow for patch bumping
+
 ## [0.0.3] - 2020-09-01
 ### Added
 - KeepPreRelease wrapper
@@ -22,6 +26,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - `nextBuild`
 - `set`
 
-[Unreleased]: https://github.com/f3ath/dart-version-manipulation/compare/0.0.3...HEAD
+[Unreleased]: https://github.com/f3ath/dart-version-manipulation/compare/0.0.4...HEAD
+[0.0.4]: https://github.com/f3ath/dart-version-manipulation/compare/0.0.3...0.0.4
 [0.0.3]: https://github.com/f3ath/dart-version-manipulation/compare/0.0.2...0.0.3
 [0.0.2]: https://github.com/f3ath/dart-version-manipulation/compare/0.0.1...0.0.2
--- test/version_change_test.dart
+++ test/version_change_test.dart
@@ -1,6 +1,5 @@
 import 'package:pub_semver/pub_semver.dart';
 import 'package:test/test.dart';
-import 'package:version_manipulation/mutations.dart';
 import 'package:version_manipulation/version_extension.dart';
 
 void main() {
@@ -33,49 +32,6 @@ void main() {
     test('in a complex build the first numeric part gets incremented', () {
       expect(Version.parse('1.2.3+foo.1.2.3.bar').nextBuild.toString(),
           '1.2.3+foo.2.0.0.bar');
-    });
-  });
-  group('Mutations', () {
-    test('BumpBreaking', () {
-      expect(BumpBreaking()(Version.parse('0.2.3+foo')).toString(), '0.3.0');
-    });
-    test('BumpMajor', () {
-      expect(BumpMajor()(Version.parse('0.2.3+foo')).toString(), '1.0.0');
-    });
-    test('BumpMinor', () {
-      expect(BumpMinor()(Version.parse('0.2.3+foo')).toString(), '0.3.0');
-    });
-    test('BumpPatch', () {
-      expect(BumpPatch()(Version.parse('0.2.3+foo')).toString(), '0.2.4');
-    });
-    test('BumpBuild', () {
-      expect(BumpBuild()(Version.parse('0.2.3+foo')).toString(), '0.2.3+foo.1');
-    });
-    test('KeepBuild', () {
-      expect(KeepBuild(BumpBreaking())(Version.parse('0.2.3+foo')).toString(),
-          '0.3.0+foo');
-      expect(KeepBuild(BumpBreaking())(Version.parse('0.2.3')).toString(),
-          '0.3.0');
-    });
-    test('KeepPreRelease', () {
-      expect(
-          KeepPreRelease(BumpBreaking())(Version.parse('0.2.3-alpha-foo.1.2.3'))
-              .toString(),
-          '0.3.0-alpha-foo.1.2.3');
-      expect(KeepPreRelease(BumpBreaking())(Version.parse('0.2.3')).toString(),
-          '0.3.0');
-    });
-    test('KeepPreRelease and KeepBuild', () {
-      expect(
-          KeepBuild(KeepPreRelease(BumpBreaking()))(
-                  Version.parse('0.2.3-alpha-foo.1.2.3+42'))
-              .toString(),
-          '0.3.0-alpha-foo.1.2.3+42');
-      expect(
-          KeepPreRelease(KeepBuild(BumpBreaking()))(
-                  Version.parse('0.2.3-alpha-foo.1.2.3+42'))
-              .toString(),
-          '0.3.0-alpha-foo.1.2.3+42');
     });
   });
 }
